d91a7d533a3b4998e814e85058a419dcd8a24b73
Zo Randrianarivo <zo.randrianarivo@gmail.com>
2022-01-20 12:17:02 +0300

    Introduction to python model using postgresql database, first model product

---
+++ wsgi.py
@@ -0,0 +1,17 @@
+# wsgi.py
+# pylint: disable=missing-docstring
+from flask_migrate import Migrate
+from flask import Flask
+from config import Config
+app = Flask(__name__)
+app.config.from_object(Config)
+
+from flask_sqlalchemy import SQLAlchemy
+db = SQLAlchemy(app)
+
+from models import Product
+migrate = Migrate(app, db)
+
+@app.route('/hello', methods=['GET'])
+def hello():
+    return "Hello World!", 200
