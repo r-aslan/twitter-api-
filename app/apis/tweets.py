a54edd7a9d6d7f370d8b3db245273f98784fdefa
Zo Randrianarivo <zo.randrianarivo@gmail.com>
2022-01-20 10:10:00 +0300

    Solution using models api

--- app/apis/tweets.py
+++ app/apis/tweets.py
@@ -3,18 +3,23 @@
 
 from flask_restx import Namespace, Resource, fields
 from app.db import tweet_repository
-from flask import jsonify
-import json
 
 api = Namespace('tweets')
 
+tweet = api.model('Tweet', {
+    'id': fields.Integer,
+    'text': fields.String,
+    'created_at': fields.DateTime
+})
+
 @api.route('/<int:id>')
 @api.response(404, 'Tweet not found')
+@api.param('id', 'The tweet unique identifier')
 class TweetResource(Resource):
+    @api.marshal_with(tweet)
     def get(self, id):
         tweet = tweet_repository.get(id)
         if tweet is None:
             api.abort(404)
-        else: 
-            tweet_id = {"id": tweet.id, "text": tweet.text, "created_at": tweet.created_at}
-            return jsonify(tweet_id)
+        else:
+            return tweet
