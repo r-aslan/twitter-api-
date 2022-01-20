82ca3ae23a03bc0b927481d0340a6e1c7785804f
Zo Randrianarivo <zo.randrianarivo@gmail.com>
2022-01-20 13:15:25 +0300

    READ a product

--- wsgi.py
+++ wsgi.py
@@ -3,7 +3,7 @@
 BASE_URL = '/api/v1'
 
 from flask_migrate import Migrate
-from flask import Flask
+from flask import Flask, request
 from config import Config
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -16,6 +16,7 @@ ma = Marshmallow(app)  # NEW LINE
 
 from models import Product
 from schemas import many_product_schema
+from schemas import one_product_schema
 migrate = Migrate(app, db)
 
 @app.route('/hello', methods=['GET'])
@@ -26,3 +27,8 @@ def hello():
 def get_many_product():
     products = db.session.query(Product).all() # SQLAlchemy request => 'SELECT * FROM products'
     return many_product_schema.jsonify(products), 200
+
+@app.route(f'{BASE_URL}/product/<id>', methods=['GET'])
+def get_one_product(id):
+    product = db.session.query(Product).get(id)
+    return one_product_schema.jsonify(product), 200
